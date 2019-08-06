e94919eace960b247650bfd4b82cb9d6eabccda9
81aksu9a <81aksu9a@imyeonglag-ui-MacBookPro.local>
2019-08-06 23:00:02 +0900

    0907

---
+++ app/views/tile/paging.php
@@ -0,0 +1,38 @@
+
+
+<?php
+$pageinfo = &$model["pageinfo"];
+
+
+
+$page=$pageinfo["page"];
+$totalpagecnt = $pageinfo["totalpagecnt"];
+
+
+$curviewpagestart = $pageinfo["curviewpagestart"];
+$curviewpageend = $pageinfo["curviewpageend"];
+
+$pageurl = $pageinfo["pageurl"];
+
+
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<?php
+    if( $curviewpagestart < $curviewpageend )
+    {
+?>
+
+        <div class="pagination">
+            <?php for($i=$curviewpagestart;$i<$curviewpageend+1;$i++){?>
+		    <a href="<?php echo $pageurl;?>&page=<?php echo $i;?>"><?php echo $i;?></a>
+		    <?php }?>
+        </div>
+<?php
+    }
+?>
