40804c91a05744f785b29345d5ecaa6525b4bd77
Taekyeong Kim <tekiteki@naver.com>
2019-08-12 15:43:49 +0900

    script for carousel

--- app/views/p_main.php
+++ app/views/p_main.php
@@ -64,7 +64,29 @@
           </div>
         </div>
       </section>
-    
+
+  <!-- for Carousel -->  
+    <script>
+      var introCarousel = $(".carousel");
+  var introCarouselIndicators = $(".carousel-indicators");
+  introCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>") :
+    introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+
+    $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') +"')");
+    $(this).children('.carousel-background').remove();
+  });
+
+  $(".carousel").swipe({
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+      if (direction == 'left') $(this).carousel('next');
+      if (direction == 'right') $(this).carousel('prev');
+    },
+    allowPageScroll:"vertical"
+  });
+  </script>
+
 
     <!-- / INDEX BANNER / -->
     <section id="index_banner">
