0b2c9f0700f988b8ea854fc2cc9a118cd5c16ec7
Taekyeong Kim <tekiteki@naver.com>
2019-08-23 19:59:17 +0900

    #82 footer margin

--- app/views/tile/footer.php
+++ app/views/tile/footer.php
@@ -2,15 +2,15 @@
             <div class="footer_logo"><img src="img/logo.png"></div>
             <div class="add_box row">
                 <div class="add_view">
-                    <p>PREMIUM TILE GALLERY TEL|02-416-0417 FAX|02-416-6292</p>
+                    <p>PREMIUM TILE GALLERY <span style="margin-left:15px;">TEL|02-416-0417 FAX|02-416-6292</span></p>
                     <p>(주)진영코리아 | 서울시 송파구 삼전로2길 3,진영빌딩(삼전동 38-1)</p>
                 </div>
                 <div class="add_none">
-                    <p>논현 TILE PLAZA TEL|02-543-5117 FAX|02-543-5104</p>
+                    <p>논현 TILE PLAZA <span style="margin-left:15px;">TEL|02-543-5117 FAX|02-543-5104</span></p>
                     <p>서울특별시 강남구 학동로24길 16 (논현동 127-7)</p>
                 </div>
                 <div class="add_none">
-                    <p>이천 제1물류센터 TEL|031-631-0951 FAX|031-631-0952 경기도 이천시 모가면 송곡2리 166-5</p>
+                    <p>이천 제1물류센터 <span style="margin-left:15px;">TEL|031-631-0951 FAX|031-631-0952 경기도 이천시 모가면 송곡2리 166-5</span></p>
                     <p>삼패 제2물류센터 경기도 남양주시 경강로334번길 48-12 (삼패동 445-4)</p>
                 </div>
             </div>
