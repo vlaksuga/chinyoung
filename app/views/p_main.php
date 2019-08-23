a8951bb2ab4214095e0473f06443bc9ea95e6aa0
Taekyeong Kim <tekiteki@naver.com>
2019-08-23 22:02:17 +0900

    #77 GRID

--- app/views/p_main.php
+++ app/views/p_main.php
@@ -99,14 +99,16 @@
             <p>-20-YEAR GUARANTEE, PROTECT ANTI-BACTERIAL</p>
             <p>THE ESSENCE OF ITALIAN TILES, STANDARDS OF HIGH-END PRODUCTS.<br>
                EXPERIENCE COTTO D'ESTE EXCLUSIVELY DISTRIBUTED BY CHINYOUNG.</p>
-        </div>
-        <div>
+          </div>
+          <div>
           <h6>코토데스테<h6>
             <p>아주 두껍거나, 아주 얇거나 20년 내구성 보장, 프로텍트 항균 이탈리아 타일의 정수, 하이-엔드의 표본. 진영코리아의 대표 브랜드 코토데스테로 경험하세요.</p>
+</div>
+         <div>
+            <div class="more"><a href="./?m=l&p=cotto">VIEW MORE</a></div>
+         </div>
 
-            <div>
-                <div class="more"><a href="./?m=l&p=cotto">VIEW MORE</a></div>
-            </div>
+        </div>
 
         </div>
         <div class="banner_right"></div>
