fe31d7c3f73934a6a536b437c4b4e34310f7d10f
Taekyeong Kim <tekiteki@naver.com>
2019-08-23 18:19:25 +0900

    #74 ABOUT

--- app/views/p_projects.php
+++ app/views/p_projects.php
@@ -16,7 +16,7 @@
                             <li><a href="?&m=projects">ALL</a></li>
                             <li><a <? if($cate=="indoor") {?>class="active"<? }?> href="?&m=projects&cate=indoor">INDOOR</a></li>
                             <li><a <? if($cate=="outdoor") {?>class="active"<? }?> href="?&m=projects&cate=outdoor">OUTDOOR</a></li>
-                            <li><a <? if($cate=="residential") {?>class="active"<? }?> href="?&m=projects&cate=residential">RESIDENTIAL</a></li>
+                            <li><a <? if($cate=="residential") {?>class="active"<? }?> href="?&m=projects&cate=residential">RESIDENTIAL</a></li> 
                             <li><a <? if($cate=="commercial") {?>class="active"<? }?> href="?&m=projects&cate=commercial">COMMERCIAL</a></li>
                             <li><a <? if($cate=="public") {?>class="active"<? }?> href="?&m=projects&cate=public">PUBLIC</a></li>
                             <li><a <? if($cate=="furnishing") {?>class="active"<? }?> href="?&m=projects&cate=furnishing">FURNISHING</a></li>
