088df03805e845986dad27b1f1c159cf5badca64
81aksu9a <81aksu9a@imyeonglag-ui-MacBookPro.local>
2019-08-26 15:03:01 +0900

    #64 add class cotto for cotto

--- app/views/p_products.php
+++ app/views/p_products.php
@@ -27,7 +27,7 @@
         <!-- / PRODUCTS TILES / -->
         <section id="products_tiles" class="sec_gap">
 
-            <div class="group">
+            <div class="group <?php if( trim($_GET["p"]) == "cotto" ) echo "cotto";?>">
                 <ul>
                     <li data-p="ALL"><a href="./?m=l">ALL</a></li>
                     <li data-p="kerlite"><a href="./?m=l&p=kerlite">KERLITE/BIG SLAB</a></li>
